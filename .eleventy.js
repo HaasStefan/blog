23bc76500be528a5ad392d04cc8c09edd90b1243
Stefan Haas <stefan.haas.privat@gmail.com>
2022-11-01 21:10:59 +0100

    base styling

--- .eleventy.js
+++ .eleventy.js
@@ -1,9 +1,22 @@
-module.exports = function(eleventyConfig) {
+const pluginRev = require("eleventy-plugin-rev");
+const eleventySass = require("eleventy-sass");
 
-    return {
-        dir: {
-          input: "src",
-          output: "public"
-        }
-      };
-}
+module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(pluginRev);
+  eleventyConfig.addPlugin(eleventySass, {
+    sass: {
+      style: "compressed",
+      sourceMap: false
+    },
+    rev: true
+  });
+
+  eleventyConfig.addPassthroughCopy("src/assets");
+
+  return {
+    dir: {
+      input: "src",
+      output: "dist",
+    },
+  };
+};
