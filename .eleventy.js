f7dfc50a119720a0db7316efdcbf90f84ca71f1c
Stefan Haas <stefan.haas.privat@gmail.com>
2022-11-07 00:10:42 +0100

    date filter + post

--- .eleventy.js
+++ .eleventy.js
@@ -2,6 +2,7 @@ const pluginRev = require("eleventy-plugin-rev");
 const eleventySass = require("eleventy-sass");
 const esbuild = require("esbuild");
 const inspect = require("util").inspect;
+const {DateTime} = require("luxon");
 
 module.exports = function (eleventyConfig) {
   eleventyConfig.addWatchTarget('./tailwind.config.js')
@@ -24,6 +25,10 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addFilter("date", (date) => {
     return new Intl.DateTimeFormat('en-US').format(new Date(date))
+  });
+
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
   });
 
   eleventyConfig.addFilter("threeOrMore", (n) => {
