1daeef85e445f8e8dd3a8a1fbab037166a209456
Stefan Haas <stefan.haas.privat@gmail.com>
2023-06-11 19:34:27 +0200

    updates

--- src/js/typing.js
+++ src/js/typing.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js';
 
 var typed = new Typed(".typing", {
-  strings: ["Web Developer^1000", "Blogger^1000", "Angular Trainer^1000", "Freelancer^1000"],
+  strings: ["Angular Freelancer^1000", "Nx Champion^1000", "Angular Trainer^1000", "Angular Consultant^1000", "Blogger^1000"],
   typeSpeed: 40,
   loop: true
 });
