88700c0a543fcbc144237150c076129add8290de
Stefan Haas <stefan.haas.privat@gmail.com>
2023-10-25 09:41:19 +0200

    freelancermap link

--- src/js/typing.js
+++ src/js/typing.js
@@ -1,7 +1,7 @@
 import Typed from 'typed.js';
 
 var typed = new Typed(".typing", {
-  strings: ["Angular Trainer^1000", "Nx Champion^1000", "Angular Freelancer^1000", "Angular Consultant^1000"],
+  strings: ["Freelancer^1000", "Nx Champion^1000", "Blogger^1000", "Angular Enthusiast^1000"],
   typeSpeed: 40,
   loop: true
 });
